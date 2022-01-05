469c9d06b674342e6016c2aff33d966f70792146
heeo <heeo.8986@kakaocorp.com>
2022-01-05 16:20:29 +0900

    #2.5 UpdateDTO -> 부분타입(partial type) 사용 (npm i @nestjs/mapped-types)

--- src/movies/dto/update-movie.dto.ts
+++ src/movies/dto/update-movie.dto.ts
@@ -1,12 +1,4 @@
-import { IsNumber, IsString } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateMovieDto } from './create-movie.dto';
 
-export class UpdateMovieDto {
-  @IsString()
-  readonly title?: string; // ?는 필수가 아니라는 뜻
-
-  @IsNumber()
-  readonly year?: number;
-
-  @IsString({ each: true })
-  readonly genres?: string[];
-}
+export class UpdateMovieDto extends PartialType(CreateMovieDto) {}
