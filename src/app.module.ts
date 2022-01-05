73c26e5fe2f8a46952e4d3f9cfbbdd768325d479
heeo <heeo.8986@kakaocorp.com>
2022-01-05 12:28:36 +0900

    #1.2 services

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,10 @@ import { AppService } from './app.service';
 @Module({
   imports: [],
   controllers: [AppController], // url을 가져오고 함수를 실행(함수로 매핑)
-  providers: [AppService],
+  providers: [AppService], // 비즈니스 로직을 실행
 })
+
+// AppModule은 루트모듈의 역할로 하나만 존재하며, 내가 만들 모든 apo를 import함
+// 왜냐하면 AppModule은 main.ts에서 NestJs가 내 어플리케이션을 만들기 위해 이용하는 것이기 때문
+// const app = await NestFactory.create(AppModule);
 export class AppModule {}
