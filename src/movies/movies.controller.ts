ee1082c5c4004c618ef9076a62cbbc43f1a8e550
heeo <heeo.8986@kakaocorp.com>
2022-01-05 13:08:02 +0900

    #2.0 movie controller CRUD 생성 및 insomnia 실습

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+  // http://localhost:3000/movies
+  @Get()
+  getAll() {
+    return 'This will return all movies';
+  }
+
+  // http://localhost:3000/movies/1
+  @Get('/:id')
+  getOne(@Param('id') movieId: string) {
+    // @Get의 id 네이밍과 와 @Param의 id 네이밍은 같아야함, movieId 부분은 달라도 됨
+    return `This will return one movie with the id ${movieId}`;
+  }
+
+  @Post()
+  create() {
+    return 'This will create a movie';
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: string) {
+    return `This will delete a movie with the id ${movieId}`;
+  }
+
+  // Patch는 리소스의 일부분만, Put은 하면 모든 리소스 업데이트
+  @Patch('/:id')
+  patch(@Param('id') movieId: string) {
+    return `This will patch a movie with the id ${movieId}`;
+  }
+}
