102c70988f1d66b402fc7bacbdbe92e398213ae9
heeo <heeo.8986@kakaocorp.com>
2022-01-07 14:01:12 +0900

    #4.2~#4.3 e2eTest > testing PATCH and DELETE movies id

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -2,7 +2,6 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/Movie.entity';
-
 @Injectable()
 export class MoviesService {
   private movies: Movie[] = [];
