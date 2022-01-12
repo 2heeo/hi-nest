ab7489afaf6d595434d62c33b68fe7a035bee58e
heeo <heeo.8986@kakaocorp.com>
2022-01-12 18:16:05 +0900

    users module, service and controller 예시 설정 중 / typeORM 세팅 중

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,1 +1,15 @@
-export class UserEntity {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 25 })
+  fullName: string;
+
+  @Column('date')
+  birthday: Date;
+
+  @Column()
+  isActive: boolean;
+}
